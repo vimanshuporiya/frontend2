349479c5ca218dacb74175003e2b8896a8595166
vimanshu <Vimanshuporiya123@gmail.com>
2024-02-14 03:59:01 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
+  process.env.NODE_ENV === 'develeopment' ? 'https://propsback2.onrender.com' : '';
 export const BASE_URL = 'https://propsback2.onrender.com'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
